0d3c6d131a0c3afd5a84c71bb409413435d47867
Jorge <jlopezgarciagt@gmail.com>
2023-09-17 12:32:14 -0600

    detalles

--- aggregator.js
+++ aggregator.js
@@ -1,9 +1,17 @@
 import {sumar, restar, multiplicar, dividir} from './modules/calculos.js';
+import {generatePassword} from './modules/passwords.js';
+import {celsiusToFahrenheit,fahrenheitToCelsius } from './modules/conversion.js'
+import {initGallery} from './modules/galeria.js'
+
 export {
     sumar,
     restar,
     multiplicar,
-    dividir
+    dividir,
+    generatePassword,
+    celsiusToFahrenheit,
+    fahrenheitToCelsius,
+    initGallery
 }
 
 
